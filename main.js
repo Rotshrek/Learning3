a7793e03b86effbd025cc797766f37c4f1336dd5
Andrés <Trazo263@gmail.com>
2017-04-06 14:07:41 -0300

    Carousel Positioning

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
-var categ = ["Movies", "Books", "Musics", "Series", "Cars", "Instruments", "Small Animals", "Danis", "Big Animals", "Dinosaurs", "Pencils", "Trees", "Tomatoes", "Spunkies", "Clouds", "Catapults"];
+var categ = ["Movies", "Books", "Musics", "Series", "Cars", "Instruments", "Small Animals", "Danis", "Big Animals", "Dinosaurs", "Pencils", "Trees", "Tomatoes", "Spunkies", "Clouds", "Catapults", "Airplanes", "Bugs", "Roads"];
 var catleng = categ.length;
 var i;
