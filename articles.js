30b13fd147da917f1a62c19a856a57f05d658a69
Andrés <Trazo263@gmail.com>
2017-04-10 16:12:47 -0300

    Nuevo archivo JS

--- articles.js
+++ articles.js
@@ -1,7 +1,3 @@
-var categ = ["Movies", "Books", "Musics", "Series", "Cars", "Instruments", "Small Animals", "Danis", "Big Animals", "Dinosaurs", "Pencils", "Trees", "Tomatoes", "Spunkies", "Clouds", "Catapults", "Airplanes", "Bugs", "Roads"];
-var catleng = categ.length;
-var i;
-
 var art = [];
 var artleng = art.length;
 var art[1] = {img : "/img/alosaurio.jpg", desc : "Soy un alosaurio"};
