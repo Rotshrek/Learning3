ac1fab2186cf1aad59d14cf7bf92f05929041b46
Andrés <Trazo263@gmail.com>
2017-04-11 19:33:53 -0300

    Carrusel con Jquery

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 var categ = ["Movies", "Books", "Musics", "Series", "Cars", "Instruments", "Small Animals", "Danis", "Big Animals", "Dinosaurs", "Pencils", "Trees", "Tomatoes", "Spunkies", "Clouds", "Catapults", "Airplanes", "Bugs", "Roads"];
 var catleng = categ.length;
 var i;
-var art = [
-	{name: "Alosaurio", img : "/img/alosaurio.jpg", desc : "Soy un alosaurio"},
-	{name: "Tiranosaurio", img : "/img/tiranosaurio.jpg", desc : "Soy un tiranosaurio"},
-	{name: "Triceratops", img : "/img/triceratops.jpg", desc : "Soy un triceratops"},
-	{name: "Anquilosaurio", img : "/img/anquilosaurio.jpg", desc : "Soy un anquilosaurio"},
-	{name: "Brontosaurio", img : "/img/brontosaurio.jpg", desc : "Soy un brontosaurio"}
-];
-var artleng = art.length;
+
+$(function() { alert('Hola, Jquery funciona') 
+});
+
+$(document).ready(function(){
+	$('.carus').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 300
+	});
+});
